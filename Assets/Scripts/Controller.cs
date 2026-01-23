bb52d78ba7aeb3f96ec055f69f32c5a221ab994a
SHERNET\matosse <matosse@sheridancollege.ca>
2026-01-23 17:27:11 -0500

    Lesson on collections on controllers

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,21 +1,79 @@
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
 
 public class Controller : MonoBehaviour
 {
     public float moveSpeed;
     public float rotateSpeed;
 
+    public SpriteRenderer spriteRenderer;
+    public Color startingColor;
+
+    public List<SpriteRenderer> controllableRenderers;
+    public List<Transform> controlledTransforms;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        bool isInisdeSprite = spriteRenderer.bounds.Contains(transform.position);
+
+        controlledTransforms.Add(transform);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 currentMousePosition = Mouse.current.position.ReadValue();
+        Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(currentMousePosition);
+        worldMousePosition.z = 0;
+
+        bool isLeftMousePressed = Mouse.current.leftButton.wasPressedThisFrame;
+        if (isLeftMousePressed)
+        {
+            //Find any renderers that are currently hovered over
+
+            //Iterate over all of the elements and determine if any of them are hovered over
+            for (int i = 0; i < controllableRenderers.Count; i++)
+            {
+                SpriteRenderer currentSpriteRenderer = controllableRenderers[i];
+                bool isHovered = controllableRenderers[i].bounds.Contains(worldMousePosition);
+                if (isHovered)
+                {
+                    controlledTransforms.Add(controllableRenderers[i].transform);
+                }
+            }
+        }
+
+        for (int i = 0; i < controlledTransforms.Count; i++)
+        {
+            Transform currentTransform = controlledTransforms[i];
+            bool leftArrowHeld = Keyboard.current.leftArrowKey.isPressed;
+            bool rightArrowHeld = Keyboard.current.rightArrowKey.isPressed;
+            bool upArrowHeld = Keyboard.current.upArrowKey.isPressed;
+            bool downArrowHeld = Keyboard.current.downArrowKey.isPressed;
+
+            if (leftArrowHeld)
+            {
+                currentTransform.eulerAngles += currentTransform.forward * rotateSpeed * Time.deltaTime;
+            }
+            if (rightArrowHeld)
+            {
+                currentTransform.eulerAngles -= currentTransform.forward * rotateSpeed * Time.deltaTime;
+            }
+            if (upArrowHeld)
+            {
+                currentTransform.position += currentTransform.up * moveSpeed * Time.deltaTime;
+            }
+            if (downArrowHeld)
+            {
+                currentTransform.position -= currentTransform.up * moveSpeed * Time.deltaTime;
+            }
+        }
+
         //bool leftIsHeld = Mouse.current.leftButton.isPressed;
         //if (leftIsHeld)
         //{
@@ -36,26 +94,26 @@ public class Controller : MonoBehaviour
 
         //bool upIsPressed = Keyboard.current.wKey.isPressed;
 
-        bool leftArrowHeld = Keyboard.current.leftArrowKey.isPressed;
-        bool rightArrowHeld = Keyboard.current.rightArrowKey.isPressed;
-        bool upArrowHeld = Keyboard.current.upArrowKey.isPressed;
-        bool downArrowHeld = Keyboard.current.downArrowKey.isPressed;
+        //bool leftArrowHeld = Keyboard.current.leftArrowKey.isPressed;
+        //bool rightArrowHeld = Keyboard.current.rightArrowKey.isPressed;
+        //bool upArrowHeld = Keyboard.current.upArrowKey.isPressed;
+        //bool downArrowHeld = Keyboard.current.downArrowKey.isPressed;
 
-        if (leftArrowHeld)
-        {
-            transform.eulerAngles += transform.forward * rotateSpeed * Time.deltaTime;
-        }
-        if (rightArrowHeld)
-        {
-            transform.eulerAngles -= transform.forward * rotateSpeed * Time.deltaTime;
-        }
-        if (upArrowHeld)
-        {
-            transform.position += transform.up * moveSpeed * Time.deltaTime;
-        }
-        if (downArrowHeld)
-        {
-            transform.position -= transform.up * moveSpeed * Time.deltaTime;
-        }
+        //if (leftArrowHeld)
+        //{
+        //    transform.eulerAngles += transform.forward * rotateSpeed * Time.deltaTime;
+        //}
+        //if (rightArrowHeld)
+        //{
+        //    transform.eulerAngles -= transform.forward * rotateSpeed * Time.deltaTime;
+        //}
+        //if (upArrowHeld)
+        //{
+        //    transform.position += transform.up * moveSpeed * Time.deltaTime;
+        //}
+        //if (downArrowHeld)
+        //{
+        //    transform.position -= transform.up * moveSpeed * Time.deltaTime;
+        //}
     }
 }
